e42675a21d508bf12e6402c4f23c244817bbd1e6
CUFXyz <belokurp@yandex.ru>
2025-05-31 23:39:24 +0700

    Added multiple pages scraping

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	flagger "scrappyscrap/internal/Flagger"
 	scrapper "scrappyscrap/internal/Scrapper"
 )
@@ -8,5 +9,14 @@ import (
 func main() {
 	flags := flagger.FlaggerSetup().Do()
 	Scrapper := scrapper.NewScrapper(flags)
-	Scrapper.ScrapPage("p")
+	var neededElement string
+	fmt.Println("Enter element what you want to parse: ")
+	fmt.Scan(&neededElement)
+	switch len(flags) {
+	case 1:
+		Scrapper.ScrapPage(neededElement)
+	default:
+		Scrapper.ScrapPages(neededElement)
+
+	}
 }
--- internal/Scrapper/Scrapper.go
+++ internal/Scrapper/Scrapper.go
@@ -1,7 +1,7 @@
 package scrapper
 
 import (
-	"log"
+	"fmt"
 	"os"
 	"strings"
 
@@ -21,19 +21,48 @@ func NewScrapper(links []string) *Scrapper {
 
 // Scrapping only one page, put an needed element into arguments
 // Making txt file with parsed text with element from argument
-func (scrap *Scrapper) ScrapPage(neededElement string) {
+func (scrap *Scrapper) ScrapPage(neededElement string) error {
 	var data []string
 	page := rod.New().NoDefaultDevice().MustConnect().MustPage(scrap.links[0]).MustWaitLoad()
 	defer page.MustClose()
-	paragraphs := page.MustElements(neededElement)
-	for _, elem := range paragraphs {
+	elements := page.MustElements(neededElement)
+	for _, elem := range elements {
 		data = append(data, elem.MustText())
 	}
-	formatteddata := strings.Join(data, " ")
+	formatteddata := strings.Join(data, "\n")
 	file, err := os.Create("Result.txt")
 	if err != nil {
-		log.Fatalf("%v", err)
+		return fmt.Errorf("fail at creating file")
 	}
 	defer file.Close()
 	file.Write([]byte(formatteddata))
+	return nil
+}
+
+// Scrapping multiple pages, also can scrap only one
+// That will be pretty stupid to return an error because more than 1 link given
+// By the way it also make txt file with parsed text from argument
+// It will separate result from parsing links with [Link %number of link%]
+func (scrap *Scrapper) ScrapPages(neededElement string) error {
+	var data []string
+
+	file, err := os.Create("MultipleResult.txt")
+	if err != nil {
+		return fmt.Errorf("fail at creating file")
+	}
+	defer file.Close()
+
+	for i := range scrap.links {
+		page := rod.New().NoDefaultDevice().MustConnect().MustPage(scrap.links[i]).MustWaitLoad()
+		elements := page.MustElements(neededElement)
+		data = append(data, fmt.Sprintf("\n[Link %d]\n", i+1))
+		for _, elem := range elements {
+			data = append(data, elem.MustText())
+		}
+		page.Close()
+	}
+
+	formatteddata := strings.Join(data, "\n")
+	file.Write([]byte(formatteddata))
+	return nil
 }
