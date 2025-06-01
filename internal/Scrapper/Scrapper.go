399fcf9d411094a9333bc0d638f49cec32e3c1d6
CUFXyz <belokurp@yandex.ru>
2025-06-01 17:29:36 +0700

    Added goroutine for multiple scrapping

--- internal/Scrapper/Scrapper.go
+++ internal/Scrapper/Scrapper.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"sync"
 
 	"github.com/go-rod/rod"
 )
@@ -39,12 +40,13 @@ func (scrap *Scrapper) ScrapPage(neededElement string) error {
 	return nil
 }
 
-// Scrapping multiple pages, also can scrap only one
+// Scrapping multiple links, also can scrap only one, but if you want to parse only one link, use ScrapPage(), that's a lot faster than this method
 // That will be pretty stupid to return an error because more than 1 link given
 // By the way it also make txt file with parsed text from argument
 // It will separate result from parsing links with [Link %number of link%]
 func (scrap *Scrapper) ScrapPages(neededElement string) error {
 	var data []string
+	var wg sync.WaitGroup
 
 	file, err := os.Create("MultipleResult.txt")
 	if err != nil {
@@ -52,16 +54,23 @@ func (scrap *Scrapper) ScrapPages(neededElement string) error {
 	}
 	defer file.Close()
 
+	wg.Add(len(scrap.links))
 	for i := range scrap.links {
-		page := rod.New().NoDefaultDevice().MustConnect().MustPage(scrap.links[i]).MustWaitLoad()
-		elements := page.MustElements(neededElement)
-		data = append(data, fmt.Sprintf("\n[Link %d]\n", i+1))
-		for _, elem := range elements {
-			data = append(data, elem.MustText())
-		}
-		page.Close()
+		go func(i int) {
+			defer wg.Done()
+			var newdata []string
+			page := rod.New().NoDefaultDevice().MustConnect().MustPage(scrap.links[i]).MustWaitLoad()
+			defer page.Close()
+			elements := page.MustElements(neededElement)
+			newdata = append(newdata, fmt.Sprintf("\n[Link %d]\n", i+1))
+			for _, elem := range elements {
+				newdata = append(newdata, elem.MustText())
+			}
+			result := strings.Join(newdata, " ")
+			data = append(data, result)
+		}(i)
 	}
-
+	wg.Wait()
 	formatteddata := strings.Join(data, "\n")
 	file.Write([]byte(formatteddata))
 	return nil
