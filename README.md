2db85132b2f629df0940f0860984727820385d31
CUFXyz <belokurp@yandex.ru>
2025-06-02 15:31:37 +0700

    some little changes

--- README.md
+++ README.md
@@ -15,15 +15,18 @@ make build
 For Single link
 ```Bash
 ./cmd/ScrappyScrap <link>
+Enter element what you want to parse: <element>
 ```
 For Multiple links
 ```Bash
 ./cmd/ScrappyScrap -m <link> <link> <...>
+Enter element what you want to parse: <element>
 ```
 
 For example:
 ```Bash
 ./cmd/ScrappyScrap -m https://en.wikipedia.org/wiki/Compiler https://en.wikipedia.org/wiki/Source_code
+Enter element what you want to parse: p
 ```
 That's creating txt file in main directory with separator **[Link %number of link%]**
 
--- internal/Flagger/Flagger.go
+++ internal/Flagger/Flagger.go
@@ -1,10 +1,14 @@
 package flagger
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
 // Const flags for the flagger unit
 const (
 	Multiple string = "-m"
+	Help     string = "-help"
 )
 
 type Flagger struct {
@@ -33,5 +37,17 @@ func (f *Flagger) Do() []string {
 		newFlags = append(newFlags, f.flags[0])
 	}
 
+	if f.flags[0] == Help {
+		f.Help()
+	}
+
 	return newFlags
+}
+
+func (f *Flagger) Help() {
+	fmt.Println("Usage:")
+	fmt.Println("./ScrappyScrap -help               | Help information table")
+	fmt.Println("./ScrappyScrap -m <link> <link>... | For multiple link scraping. Actually, you can scrap a single link with that flag, but not recommended, it's gonna take more time")
+	fmt.Println("./ScrappyScrap <link>              | For single link scraping")
+	os.Exit(0)
 }
